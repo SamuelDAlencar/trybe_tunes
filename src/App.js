a4dd416ffe94219f218a35552a97ab28d99149ab
SamuelDAlencar <sarl.2001@hotmail.com>
2022-01-14 12:40:40 -0300

    second requirement

--- src/App.js
+++ src/App.js
@@ -1,19 +1,71 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
+import Loading from './pages/Loading';
 import Search from './pages/Search';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import * as userAPI from './services/userAPI';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      input: '',
+      isDisabled: true,
+      isLoading: false,
+      isLoggedin: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target: { value } }) {
+    const MAX_VALUE = 3;
+
+    this.setState(({ input: value }), () => {
+      if (value.length >= MAX_VALUE) {
+        this.setState({ isDisabled: false });
+      } else {
+        this.setState({ isDisabled: true });
+      }
+    });
+  }
+
+  async handleClick() {
+    const { input } = this.state;
+
+    this.setState({ isLoading: true });
+    await userAPI.createUser({ name: input });
+    this.setState({ isLoading: false, isLoggedin: true });
+  }
+
   render() {
+    const { input, isDisabled, isLoading, isLoggedin } = this.state;
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={ Login } />
+          <Route
+            exact
+            path="/"
+            render={ () => (
+              <>
+                <Login
+                  inputHandler={ this.handleChange }
+                  input={ input }
+                  isDisabled={ isDisabled }
+                  createUser={ this.handleClick }
+                />
+                {isLoading && <Loading /> }
+                {isLoggedin && <Redirect to="/search" /> }
+              </>
+            ) }
+          />
           <Route exact path="/search" component={ Search } />
           <Route exact path="/album/:id" component={ Album } />
           <Route exact path="/favorites" component={ Favorites } />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
 
 export default class Login extends Component {
   render() {
+    const { input,
+      inputHandler,
+      isDisabled,
+      createUser,
+    } = this.props;
+
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <label htmlFor="name">
+          Nome
+          <input
+            type="text"
+            data-testid="login-name-input"
+            id="name"
+            onChange={ inputHandler }
+            value={ input }
+          />
+        </label>
+        <button
+          type="submit"
+          data-testid="login-submit-button"
+          disabled={ isDisabled }
+          onClick={ createUser }
+        >
+          Entrar
+        </button>
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  input: propTypes.string,
+  inputHandler: propTypes.func,
+  isDisabled: propTypes.bool,
+}.isRequired;
