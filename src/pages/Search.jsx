f7849f6be9f3b75487d17ef1160e275b0ffaa108
SamuelDAlencar <sarl.2001@hotmail.com>
2022-01-16 21:30:13 -0300

    sixth requirement

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from './Loading';
+import searchAlbumsAPIs from '../services/searchAlbumsAPI';
 
 export default class Search extends Component {
   constructor() {
@@ -7,10 +10,15 @@ export default class Search extends Component {
 
     this.state = {
       input: '',
+      artistName: '',
+      searchResults: [],
       isDisabled: true,
+      isLoading: false,
+      isReady: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.searchArtist = this.searchArtist.bind(this);
   }
 
   handleChange({ target: { value } }) {
@@ -18,33 +26,86 @@ export default class Search extends Component {
 
     this.setState({
       input: value,
+      artistName: value,
       isDisabled: value.length < MIN_LENGTH,
     });
   }
 
+  async searchArtist() {
+    this.setState({ isLoading: true });
+    const input = document.getElementById('search-artist');
+    const results = await searchAlbumsAPIs(input.value);
+    console.log(results);
+
+    if (results.length === 0) {
+      this.setState({ error404: true });
+    } else {
+      this.setState({
+        error404: false,
+      });
+    }
+
+    this.setState({
+      isLoading: false,
+      isReady: true,
+      searchResults: [...results],
+      input: '',
+    });
+
+    input.value = '';
+  }
+
   render() {
-    const { input, isDisabled } = this.state;
+    const {
+      input,
+      artistName,
+      isDisabled,
+      isLoading,
+      isReady,
+      searchResults,
+      error404 } = this.state;
 
     return (
       <>
         <Header />
         <div data-testid="page-search">
-          <label htmlFor="search-artist">
-            Artist
-            <input
-              data-testid="search-artist-input"
-              id="search-artist"
-              onChange={ this.handleChange }
-              value={ input }
-            />
-          </label>
-          <button
-            data-testid="search-artist-button"
-            disabled={ isDisabled }
-            type="button"
-          >
-            Pesquisar
-          </button>
+          {isLoading ? <Loading />
+            : (
+              <>
+                <label htmlFor="search-artist">
+                  Artist
+                  <input
+                    data-testid="search-artist-input"
+                    id="search-artist"
+                    onChange={ this.handleChange }
+                    value={ input }
+                  />
+                </label>
+                <button
+                  data-testid="search-artist-button"
+                  disabled={ isDisabled }
+                  type="button"
+                  onClick={ this.searchArtist }
+                >
+                  Pesquisar
+                </button>
+                {error404 && <p>Nenhum álbum foi encontrado</p>}
+                {(isReady === true && error404 === false) && (
+                  <span>
+                    Resultado de álbuns de:
+                    {' '}
+                    {artistName}
+                  </span>)}
+                {searchResults.map((result) => (
+                  <Link
+                    data-testid={ `link-to-album-${result.collectionId}` }
+                    key={ result.collectionId }
+                    to={ `/album/${result.collectionId}` }
+                  >
+                    {result.collectionName}
+                  </Link>
+                ))}
+              </>)}
         </div>
       </>
     );
